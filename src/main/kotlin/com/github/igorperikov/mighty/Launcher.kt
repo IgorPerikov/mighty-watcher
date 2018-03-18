9eda0943ba9625abfabd5096363ad2a3317eead2
Igor Perikov <perikov.igor@gmail.com>
2018-03-18 09:38:42 +0300

    more idiomatic code

--- src/main/kotlin/com/github/igorperikov/mighty/Launcher.kt
+++ src/main/kotlin/com/github/igorperikov/mighty/Launcher.kt
@@ -1,6 +1,5 @@
 package com.github.igorperikov.mighty
 
-import com.github.igorperikov.mighty.entity.Issue
 import com.github.igorperikov.mighty.external.Client
 import com.github.igorperikov.mighty.external.RestClient
 import java.io.File
@@ -20,53 +19,48 @@ object Launcher {
     fun main(args: Array<String>) {
         println("start at  ${ZonedDateTime.now(ZoneId.of("Europe/Moscow"))}")
         if (importRepositories) {
+            val languages = readResourceFile("languages")
+                .lines()
+                .map { it.toLowerCase() }
+                .toSet()
+
+            val ignoredRepos = readResourceFile("ignored-repos").lines().toSet()
+
             val repositories = client.getStarredRepositories(username)
-            val languages = mutableSetOf<String>()
-            val ignoredRepos = mutableSetOf<String>()
-
-            readResourceFile("languages")
-                .lines()
-                .forEach { languages.add(it.toLowerCase()) }
-
-            readResourceFile("ignored-repos")
-                .lines()
-                .forEach { ignoredRepos.add(it) }
+                .filter { it.hasIssues }
+                .filter { it.fullName !in ignoredRepos }
+                .filter { it.language?.toLowerCase() in languages }
 
             val file = File("src/main/resources/tracked-repos")
             file.delete()
             file.createNewFile()
 
-            repositories
-                .filter { it.hasIssues }
-                .filter { it.fullName !in ignoredRepos }
-                .filter { it.language?.toLowerCase() in languages }
-                .forEach { file.appendText(it.fullName + "\r\n") }
+            repositories.forEach { file.appendText(it.fullName + "\r\n") }
         }
 
         val labelsSet = readResourceFile("labels").lines().filter { it.isNotBlank() }.toSet()
         val ignoredIssues = readResourceFile("ignored-issues").lines().toSet()
 
-        val issues = mutableSetOf<Issue>()
-
-        readResourceFile("tracked-repos")
+        val issues = readResourceFile("tracked-repos")
             .lines()
             .filter { it.isNotBlank() }
-            .forEach { repoFullName ->
-                labelsSet.forEach { label ->
-                    val notIgnoredIssues = client.getIssues(repoFullName, label).filter { it.htmlUrl !in ignoredIssues }
-                    issues.addAll(notIgnoredIssues)
+            .flatMap { repoFullName ->
+                labelsSet.map { label ->
+                    Pair(repoFullName, label)
                 }
             }
+            .flatMap { client.getIssues(it.first, it.second) }
+            .filter { it.htmlUrl !in ignoredIssues }
+            .sortedByDescending { it.createdAt }
 
         val resultFile = File("src/main/resources/result")
         resultFile.delete()
         resultFile.createNewFile()
 
-        issues
-            .sortedByDescending { it.createdAt }
-            .forEach {
-                resultFile.appendText(it.toString() + "\r\n")
-            }
+        issues.forEach {
+            resultFile.appendText(it.toString() + "\r\n")
+        }
+
         println("finish at ${ZonedDateTime.now(ZoneId.of("Europe/Moscow"))}")
     }
 
