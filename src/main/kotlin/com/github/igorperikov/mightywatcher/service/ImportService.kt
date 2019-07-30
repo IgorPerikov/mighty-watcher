74d78f341ec9af17f8e002f5096e569d1fc19a5c
IgorPerikov <perikov.igor@gmail.com>
2019-07-30 14:53:10 +0300

    store labels at Kotlin Service

--- src/main/kotlin/com/github/igorperikov/mightywatcher/service/ImportService.kt
+++ src/main/kotlin/com/github/igorperikov/mightywatcher/service/ImportService.kt
@@ -8,8 +8,6 @@ import com.github.igorperikov.mightywatcher.entity.Repository
 import com.github.igorperikov.mightywatcher.entity.SearchTask
 import com.github.igorperikov.mightywatcher.external.GithubApiClient
 import com.github.igorperikov.mightywatcher.utils.executeInParallel
-import java.nio.file.Files
-import java.nio.file.Paths
 import java.time.Duration
 import java.time.Instant
 import java.time.ZoneOffset
@@ -17,12 +15,6 @@ import java.time.format.DateTimeFormatter
 import java.util.*
 
 class ImportService(private val githubApiClient: GithubApiClient) {
-    private val easyLabels = Files.readAllLines(
-        Paths.get(
-            javaClass.classLoader.getResource("labels")?.toURI() ?: throw RuntimeException("Labels resource not found")
-        )
-    ).filter { it.isNotBlank() }
-
     private val since = DateTimeFormatter.ISO_LOCAL_DATE_TIME
         .withLocale(Locale.ENGLISH)
         .withZone(ZoneOffset.UTC)
@@ -30,9 +22,9 @@ class ImportService(private val githubApiClient: GithubApiClient) {
 
     private val includedLanguages: Set<String> =
         System.getenv(INCLUDE_LANG_ENV_NAME)?.split(",")?.toHashSet() ?: setOf()
+
     private val excludedRepositories: Set<String> =
         System.getenv(EXCLUDE_REPOS_ENV_NAME)?.split(",")?.toHashSet() ?: setOf()
-
     fun getSearchTasks(): List<SearchTask> {
         return executeInParallel(fetchStarredRepositories()) { repository ->
             findLabelsConjunction(repository).map { label -> SearchTask(repository, label) }
@@ -49,7 +41,7 @@ class ImportService(private val githubApiClient: GithubApiClient) {
 
     private fun findLabelsConjunction(repository: Repository): List<String> {
         val repositoryLabels = getRepositoryLabels(repository).map { it.name }.toHashSet()
-        return easyLabels.filter { repositoryLabels.contains(it) }
+        return LabelsService.getEasyLabels().filter { repositoryLabels.contains(it) }
     }
 
     private fun fetchStarredRepositories(): List<Repository> {
---
+++ src/main/kotlin/com/github/igorperikov/mightywatcher/service/LabelsService.kt
@@ -0,0 +1,41 @@
+package com.github.igorperikov.mightywatcher.service
+
+object LabelsService {
+    fun getEasyLabels(): List<String> {
+        return listOf(
+            "accepting PRs",
+            "adoptme",
+            "beginner",
+            "Beginner friendly",
+            "contributions welcome",
+            "difficulty:easy",
+            "easy",
+            "Easy Fix",
+            "Easy to fix",
+            "Effort-low",
+            "E-easy",
+            "E-help-wanted",
+            "E-mentor",
+            "E-medium",
+            "E-needstest",
+            "Good as First PR",
+            "good first bug",
+            "good-first-contribution",
+            "good first issue",
+            "good-first-issue",
+            "Good First Issue",
+            "good for beginners",
+            "hacktoberfest",
+            "Hacktoberfest",
+            "help wanted",
+            "help-wanted",
+            "help wanted (easy)",
+            "ideal for contribution",
+            "low hanging fruit",
+            "Meta: Good First Issue",
+            "noob friendly",
+            "PR welcome",
+            "up-for-grabs"
+        )
+    }
+}
